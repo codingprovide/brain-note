c7c1e02a6970fecfdfb171060944f25159799c12
star <otzhelen2018@gmail.com>
2024-12-25 15:09:11 +0800

    feat顯示工具列 1.預設工具為鉛筆(icon顯示藍色) 2.移動或點擊按鈕時背景更換為白色icon為藍色 3.目前使用的工具icon為藍色(如1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import Flow from "./components/Flow";
+import PrainToolsBar from "./components/PrainToolsBar"
 
 export default function App() {
-  return <Flow />;
+  return (
+    <div className="h-screen w-screen">
+      <Flow />
+      <PrainToolsBar />
+    </div>
+    
+  )
 }
